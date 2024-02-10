f8f00953885fd775c8512fb2b841e990f8588c8d
mass10 <mass10.github@gmail.com>
2024-02-10 10:58:53 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 mod application;
 mod services;
+mod util;
+
+/// 使用方法
+fn usage() {
+	let pkg_name = env!("CARGO_PKG_NAME");
+	eprintln!("Usage: {}", pkg_name);
+}
 
 /// Rust アプリケーションのエントリーポイント
 fn main() {
 	let args = std::env::args().skip(1).collect::<Vec<String>>();
 	if args.len() != 1 {
-		eprintln!("Usage: ");
+		usage();
 		std::process::exit(1);
 	}
 
 	// アプリケーションを実行
-	let app = application::create_application();
+	let mut app = application::create_application();
 	let result = app.run(&args[0]);
 	if result.is_err() {
-		eprintln!("Error: {:?}", result.err());
+		let error = result.err().unwrap();
+		let message: String = error.to_string();
+		if message == "" {
+			std::process::exit(1);
+		}
+		eprintln!("Error: {:?}", error);
+		std::process::exit(1);
 	}
 }
