b936f4e44766cc8d20f4c139acbbcc90ec3a9da5
mass10 <mass10.github@gmail.com>
2024-02-10 10:58:54 +0900

    Update application.rs

--- src/application.rs
+++ src/application.rs
@@ -3,56 +3,35 @@
 //!
 
 use crate::services;
+use crate::util;
 
-#[allow(unused)]
-fn usage() {
-	let pkg_name = env!("CARGO_PKG_NAME");
-	eprintln!("Usage: {} NAME", pkg_name);
-	std::process::exit(1);
-}
-
-/// ファイルハンドラ
-struct YarnFileAnalyzer;
-
-impl YarnFileAnalyzer {
-	/// yarn.lock の分析
-	pub fn analyze(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
-		let name = std::path::Path::new(path).file_name().unwrap().to_str().unwrap();
-		if name != "yarn.lock" {
-			return Ok(());
-		}
-
-		// 分析
-		services::analyze_yarn_lock(path)?;
-
-		return Ok(());
-	}
-}
-
+/// アプリケーションのトレイト
 pub trait Application {
 	/// アプリケーションのエントリーポイント
-	fn run(&self, path: &str) -> Result<(), Box<dyn std::error::Error>>;
+	fn run(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>>;
 }
 
+/// アプリケーションの実装
 struct ApplicationImpl;
 
 impl Application for ApplicationImpl {
 	/// アプリケーションのエントリーポイント
-	fn run(&self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
+	fn run(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>> {
 		// パッケージファイルの分析
-		let mut handler_impl = YarnFileAnalyzer {};
+		let mut handler_impl = services::PackageFileAnalyzer {};
 		let mut handler = move |s: &str| -> Result<(), Box<dyn std::error::Error>> {
 			return handler_impl.analyze(s);
 		};
 
 		// ディレクトリーを再帰的に探索します。
-		let search = services::DirectorySearch::new();
+		let search = util::DirectorySearch::new();
 		search.find_dir(path, &mut handler)?;
 
 		return Ok(());
 	}
 }
 
+/// アプリケーションを初期化します。
 pub fn create_application() -> Box<dyn Application> {
 	let instance = ApplicationImpl;
 	return Box::new(instance);
