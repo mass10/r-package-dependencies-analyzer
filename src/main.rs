6be3abd98ee7d142a0010a52fad75777c4f8573c
mass10 <mass10.github@gmail.com>
2024-02-10 09:23:39 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod application;
+mod service;
 
 /// Rust アプリケーションのエントリーポイント
 fn main() {
 	let args = std::env::args().skip(1).collect::<Vec<String>>();
 	if args.len() != 1 {
-		eprintln!("Usage: hello NAME");
+		eprintln!("Usage: ");
 		std::process::exit(1);
 	}
 
 	// アプリケーションを実行
 	let app = application::create_application();
-	let result = app.run();
+	let result = app.run(&args[0]);
 	if result.is_err() {
 		eprintln!("Error: {:?}", result.err());
 	}
